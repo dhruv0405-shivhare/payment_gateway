6a506d6ca22076c641911c14fae27cfc1835493e
Dhruv <dshivhare661@gmail.com>
2025-03-18 15:25:32 +0530

    integration

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import razorpay
 
 def index(request):
     return render(request,'index.html')
@@ -8,8 +8,12 @@ def index(request):
 def payment(request):
     if request.method == 'POST':
         amount=int(request.POST.get('amount'))
-        print(amount)
+        # print(amount)
         # print(type(amount))
-        return render(request,'index.html')
+        client = razorpay.Client(auth=("YOUR_ID", "YOUR_SECRET"))
+
+        data = { "amount": amount*100, "currency": "INR", "receipt": "order_rcptid_11" }
+        payment = client.order.create(data=data) #Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        print(payment)
     
     
